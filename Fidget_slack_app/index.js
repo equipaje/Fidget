e26b15094bcba9db7e59a29e018e478eaf9a90a0
equipaje <elise.livingston@gmail.com>
2022-08-23 15:39:26 -0400

    update readme and remove tokens & secrets

--- Fidget_slack_app/index.js
+++ Fidget_slack_app/index.js
@@ -3,10 +3,10 @@ const { App } = require("@slack/bolt");
 
 
 const app = new App({
-    token: "xoxb-3916522946278-3925615234180-YBHsQA4RcbQX0Ko3rnRBPR5E", //Find in the Oauth  & Permissions tab
-    signingSecret: "2939a9f7e2ccf5f05c30fd72c57f70ef", // Find in Basic Information Tab
+    token: "lorem", //Find in the Oauth  & Permissions tab
+    signingSecret: "ipsum", // Find in Basic Information Tab
     socketMode:true,
-    appToken: "xapp-1-A03T54CJVRQ-3908722730455-46ff87d4b0ee0cc79e77a35a22af3dfc98cec9b615a6fc506e20c32a210e38d3" // Token from the App-level Token that we created
+    appToken: "dolor" // Token from the App-level Token in Basic Info tab (give read and write scopes)
 });
 
 app.command("/square", async ({ command, ack, say }) => {
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Fidget
 
 An app that sends a message from Figma to Slack.
+
+Directions to get this running:
+
+1. FIGMA PLUGIN
+    - In Figma, Go to Plugins > Development > Manage plugins in development
+    - Add plugin > Import plugin from manifest
+    - Choose the manifest.json from the Fidget_figma_plugin file
+    - Make sure the slack webhook URL in ui.html is still valid (you can generate a new one at https://api.slack.com/apps/A03T54CJVRQ/incoming-webhooks?)
+
+
+2. SLACK APP
+    - Run the slack app in vsCode w/ Node.js
+    - Make sure the token, signing secret, app token are valid in index.js (regenerate those from https://api.slack.com/apps)
+
+
+3. Run the Figma plugin and cross your fingers that it all works
